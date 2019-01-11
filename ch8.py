682f95f73dcec753d6402e1012448afcb36dbfd6
beboptank <beboptank12@gmail.com>
2019-01-11 19:54:14 +0900

    Add solutions to ch. 8

---
+++ ch8.py
@@ -0,0 +1,15 @@
+#call a different function from the statistics module
+
+import statistics
+
+numberList = [5, 8, 10, 20, 31]
+
+statistics.harmonic_mean(numberList)
+
+
+#create a module named cubed with a function that takes a number as a parameter
+#and returns the number cubed. Import and call the function from another module.
+
+import cubed
+
+cubed.cubeNumber(3)
